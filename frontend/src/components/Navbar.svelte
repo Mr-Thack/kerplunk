a2549e15f3347d8c9c127859a2091e05f0717de5
Johnathan Cunningham <51341139+superrhodes@users.noreply.github.com>
2023-06-04 18:28:37 +0000

    Icons in Navbar

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -2,25 +2,25 @@
   import { page } from '$app/stores';
 	import { AppRail, AppRailAnchor, LightSwitch } from '@skeletonlabs/skeleton';
 
-  let navs = [['chatrooms','Chat!'],['login','Login!']];
+  let navs = [['chatrooms','Chat!','chat'],['login','Login!','login']];
 </script>
 
 
-<AppRail>
+<AppRail class="">
   <svelte:fragment slot="lead">
     <AppRailAnchor href="/">
-      <img src="/icon.jpg" alt="icon" style="width: auto; height: auto; max-height: 50%" />
+      <img src="/icon.jpg" alt="icon" class="w-auto h-auto" />
     </AppRailAnchor>
   </svelte:fragment>
   
   {#each navs as n}
     <AppRailAnchor href={n[0]} selected={$page.url.pathname === '/' + n[0]}>
-      <svelte:fragment slot="lead">(ICON)</svelte:fragment>
+      <svelte:fragment slot="lead"><span class="material-symbols-outlined">{n[2]}</span></svelte:fragment>
       <span>{n[1]}</span>
     </AppRailAnchor>
   {/each}
 
-  <div class="lightswitch">
+  <div class="lightswitch m-4">
     <LightSwitch />
   </div>
 </AppRail>
