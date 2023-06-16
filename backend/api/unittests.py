e3137f4249c42310e6ff649d31ef1d7edc7e26a4
j112er <johnathancunningham41@gmail.com>
2023-06-16 01:18:37 -0400

    Update unittests

--- backend/api/unittests.py
+++ backend/api/unittests.py
@@ -2,8 +2,8 @@
 import unittest
 from dataclasses import dataclass, field
 
-import os
-os.chdir("backend/api")
+# import os
+# os.chdir("backend/api")
 
 # ALERT: Set to true for performance test
 PERF = False
