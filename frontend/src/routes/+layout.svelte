fa00b0bcd0b37aa0c3b9fc746d08bfe213914a5b
Johnathan Cunningham <51341139+superrhodes@users.noreply.github.com>
2023-06-18 03:50:11 +0000

    Added framework for theme colors

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
 	import Navbar from '$components/Navbar.svelte';
-	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	import '../app.postcss';
+	import '../themes/theme_red.postcss';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
 
-	
-		
 	import { AppShell } from '@skeletonlabs/skeleton';
 	import { Modal, modalStore } from '@skeletonlabs/skeleton';
 	import type { ModalSettings, ModalComponent } from '@skeletonlabs/skeleton'
