32d67ead0affaae33d82bdca67561f6f16949df7
Abdul Muqeet Mohammed <mr_thack@yahoo.com>
2023-06-29 03:08:32 -0400

    Cleanup

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -21,7 +21,10 @@ const config = {
 			fallback: null,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/github-pages' : ''
+		}
 	}
 };
 export default config;
