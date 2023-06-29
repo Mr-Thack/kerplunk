1f2a708af9fe09bd0174aafa2e03c4e0d473580f
Abdul Muqeet Mohammed <mr_thack@yahoo.com>
2023-06-29 03:20:39 -0400

    Fixed base

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -21,7 +21,10 @@ const config = {
 			fallback: null,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/kerplunk' : ''
+		}
 	}
 };
 export default config;
