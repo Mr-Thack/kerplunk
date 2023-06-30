ed3582cceaa888a7a04610bff36d750fe85a270d
Abdul Muqeet Mohammed <mr_thack@yahoo.com>
2023-06-30 18:31:53 -0400

    Updates

--- _app/immutable/chunks/navigation.0345319d.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./singletons.633ce90a.js";const e=o("goto");export{e as g};
---
+++ _app/immutable/chunks/settings.e98476f1.js
@@ -0,0 +1,1 @@
+import{u as t}from"./stores.88811b96.js";import{g as r}from"./endpoint.4b649916.js";async function s(e){return await r("userme",{fields:e},t.readonce("token"))}export{s as g};
