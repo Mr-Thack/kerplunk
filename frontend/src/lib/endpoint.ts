e5c2c4bf4e8f7449d52e87ff6a7106e912209acb
Johnathan Cunningham <51341139+superrhodes@users.noreply.github.com>
2023-07-02 01:55:13 -0400

    whoops

--- frontend/src/lib/endpoint.ts
+++ frontend/src/lib/endpoint.ts
@@ -8,7 +8,7 @@ export function endpoint(endpoint: string) {
     // This wont work in prod, fix later
     return dev?
         (window.location.host.split(":")[0] + ":8000" + endpoint)
-        : ('3b4e-24-98-126-28.ngrok-free.app' + endpoint)
+        : ('2e3c-24-98-126-28.ngrok-free.app' + endpoint)
 }
 
 async function request(method: string, endPoint: string, hs: HeadersInit, body: string, token: string) {
