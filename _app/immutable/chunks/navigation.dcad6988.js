ad31316994b6c31d7a668c0195c44dee9b730a22
Johnathan Cunningham <51341139+superrhodes@users.noreply.github.com>
2023-07-20 14:56:04 +0000

    Updates

---
+++ _app/immutable/chunks/navigation.dcad6988.js
@@ -0,0 +1,1 @@
+import{h as o}from"./singletons.561afff8.js";const e=o("goto");export{e as g};
--- _app/immutable/chunks/settings.3a2d95a9.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./stores.0e991afd.js";import{g as r}from"./endpoint.a38f7290.js";async function s(e){return await r("userme",{fields:e},t.readonce("token"))}export{s as g};
