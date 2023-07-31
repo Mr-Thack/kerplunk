9089dc875189493ada80845fb7ee9c34fed9450d
j112er <johnathancunningham41@gmail.com>
2023-07-31 00:21:25 -0400

    Updates

--- _app/immutable/chunks/navigation.df7b6fbb.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./singletons.d4d8409d.js";const e=o("goto");export{e as g};
---
+++ _app/immutable/chunks/settings.f02b6370.js
@@ -0,0 +1,1 @@
+import{u as t}from"./stores.0e991afd.js";import{g as r}from"./endpoint.d927bf6c.js";async function s(e){return await r("userme",{fields:e},t.readonce("token"))}export{s as g};
