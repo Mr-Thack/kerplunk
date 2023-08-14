89361f05373c34309132d32ec8e830e4298da582
Abdul Muqeet Mohammed <mr_thack@yahoo.com>
2023-08-14 05:59:53 -0400

    Script for forwarding ports on startup

---
+++ backend/forwardsports.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function forward {
+        # Give it EXTERNAL:INTERNAL
+        local splits=(${1//:/ })
+        local external=${splits[0]}
+        local internal=${splits[1]}
+        iptables -t nat -A PREROUTING -p tcp --dport $external -j REDIRECT --to-port $internal
+        iptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport $external -j REDIRECT --to-ports $internal
+}
+
+
+forward 80:8080
+forward 443:8080
+
+# Output current forwarding rules, which should now include the 80 to 8080 redirection
+# iptables -t nat --line-numbers -n -L
+
