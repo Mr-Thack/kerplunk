8804159dd7740d2b746771445dc71e86ae39667c
ferretninja007 <97195773+ferretninja007@users.noreply.github.com>
2023-08-21 15:20:17 -0400

    Update +page.svelte
    got rid of references to remind/edmodo

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 <h1 class="h1 mt-8 mb-4">Welcome to Kerplunk!</h1>
 
-<h4 class="h4"> This is a Remind/Edmodo replacement</h4>
+<h4 class="h4"> This is an educational tool used to connect students, parents, and teachers.</h4>
 
 <h4 class="h4">Please sign up if you have not done so already!</h4>
 
