edf818acd62f2ae9cb9082bd13d5f241abc02e0f
j112er <johnathancunningham41@gmail.com>
2023-08-24 16:28:47 -0400

    chatroom fix

--- frontend/src/routes/chatrooms/+page.svelte
+++ frontend/src/routes/chatrooms/+page.svelte
@@ -114,7 +114,7 @@
 <div bind:this={chatroomList} class="m-4 overflow-auto">
   {#if chatrooms.length}
     {#each chatrooms as chatroom}
-      <div class="btn flex flex-col bg-gradient-to-br mt-2 mx-auto pr-6 w-full md:w-7/12 lg:w-5/12 text-sm lg:text-base rounded-lg" on:click={() => join(chatroom[0], chatroom[1])}>
+      <div class="btn flex flex-col bg-gradient-to-br mt-2 mx-auto pr-6 w-full md:w-7/12 lg:w-5/12 text-sm lg:text-base rounded-lg" on:click={() => join(chatroom.name, chatroom.public)}>
         <div class="p-4 variant-filled-primary w-full ml-2 rounded-t-lg">{chatroom.name}</div>
         <div class="flex flex-row justify-center p-4 variant-ghost-primary w-full rounded-b-lg"><p class="material-symbols-outlined">person</p><p>{Object.keys(chatroom.users).length}</p></div>
       </div>
